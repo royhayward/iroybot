1beb0f669d0e62cc1cd825aa6b8e7b01e7f11229
Roy Hayward <roy@ponderize.me>
2024-08-14 15:16:59 -0600

    adding a blog to the progect. Sort of.

---
+++ blog/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+import Posts from "./components/Posts";
+import Navbar from "./components/BlogNav"
+
+const App = () => {
+  return (
+      <div className="main-container" style={{backgroundColor: "aliceblue"}}>
+          <Navbar />
+          <Posts />
+      </div>
+  );
+};
+
+export default App;
