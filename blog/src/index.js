1382057adbea07df1dd059a40b5d825b9e4d2b55
Roy Hayward <roy@ponderize.me>
2024-08-19 11:32:49 -0600

    new build

--- blog/src/index.js
+++ blog/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -14,4 +15,14 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+// const newEvent(() => {
+//   const requestOptions = {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json', 'topic': 'topic' },
+//     body: JSON.stringify(body)
+//   };
+//   fetch('http://url', requestOptions)
+//     .then(response => response.json());
+// });
